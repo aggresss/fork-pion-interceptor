7254303c94fca079d6fcb355113feabf195ca622
aggresss <aggresss@163.com>
2024-02-22 15:00:52 +0800

    Support FlexFEC-03

--- pkg/flexfec/encoder_interceptor.go
+++ pkg/flexfec/encoder_interceptor.go
@@ -4,6 +4,9 @@
 package flexfec
 
 import (
+	"strings"
+	"sync"
+
 	"github.com/pion/interceptor"
 	"github.com/pion/rtp"
 )
@@ -11,9 +14,17 @@ import (
 // FecInterceptor implements FlexFec.
 type FecInterceptor struct {
 	interceptor.NoOp
-	flexFecEncoder     FlexEncoder
-	packetBuffer       []rtp.Packet
 	minNumMediaPackets uint32
+
+	streams   map[uint32]*fecStream
+	streamsMu sync.Mutex
+}
+
+type fecStream struct {
+	flexFecEncoder FlexEncoder
+	packetBuffer   []rtp.Packet
+
+	fecRtpWriter interceptor.RTPWriter
 }
 
 // FecOption can be used to set initial options on Fec encoder interceptors.
@@ -35,11 +46,18 @@ func (r *FecInterceptorFactory) NewInterceptor(_ string) (interceptor.Intercepto
 	// Min num media packets to encode FEC -> 5
 	// Min num fec packets -> 1
 
-	interceptor := &FecInterceptor{
-		packetBuffer:       make([]rtp.Packet, 0),
+	i := &FecInterceptor{
 		minNumMediaPackets: 5,
+		streams:            map[uint32]*fecStream{},
 	}
-	return interceptor, nil
+
+	for _, opt := range r.opts {
+		if err := opt(i); err != nil {
+			return nil, err
+		}
+	}
+
+	return i, nil
 }
 
 // BindLocalStream lets you modify any outgoing RTP packets. It is called once for per LocalStream. The returned method
@@ -47,33 +65,56 @@ func (r *FecInterceptorFactory) NewInterceptor(_ string) (interceptor.Intercepto
 func (r *FecInterceptor) BindLocalStream(info *interceptor.StreamInfo, writer interceptor.RTPWriter) interceptor.RTPWriter {
 	// Chromium supports version flexfec-03 of existing draft, this is the one we will configure by default
 	// although we should support configuring the latest (flexfec-20) as well.
-	r.flexFecEncoder = NewFlexEncoder03(info.PayloadType, info.SSRC)
+	r.streamsMu.Lock()
+	defer r.streamsMu.Unlock()
 
-	return interceptor.RTPWriterFunc(func(header *rtp.Header, payload []byte, attributes interceptor.Attributes) (int, error) {
-		r.packetBuffer = append(r.packetBuffer, rtp.Packet{
-			Header:  *header,
-			Payload: payload,
-		})
-
-		// Send the media RTP packet
-		result, err := writer.Write(header, payload, attributes)
-
-		// Send the FEC packets
-		var fecPackets []rtp.Packet
-		if len(r.packetBuffer) == int(r.minNumMediaPackets) {
-			fecPackets = r.flexFecEncoder.EncodeFec(r.packetBuffer, 2)
-
-			for _, fecPacket := range fecPackets {
-				fecResult, fecErr := writer.Write(&fecPacket.Header, fecPacket.Payload, attributes)
-
-				if fecErr != nil && fecResult == 0 {
-					break
-				}
-			}
-			// Reset the packet buffer now that we've sent the corresponding FEC packets.
-			r.packetBuffer = nil
+	if info.Attributes.Get("flexfec-03") != nil {
+		f := &fecStream{
+			packetBuffer: make([]rtp.Packet, 0),
 		}
+		r.streams[info.SSRC] = f
 
-		return result, err
-	})
+		return interceptor.RTPWriterFunc(func(header *rtp.Header, payload []byte, attributes interceptor.Attributes) (int, error) {
+			// Store media RTP packet
+			if f.fecRtpWriter != nil {
+				f.packetBuffer = append(f.packetBuffer, rtp.Packet{
+					Header:  *header,
+					Payload: payload,
+				})
+			}
+			// Send the media RTP packet
+			result, err := writer.Write(header, payload, attributes)
+			// Send the FEC packets
+			var fecPackets []rtp.Packet
+			if len(f.packetBuffer) == int(r.minNumMediaPackets) && f.fecRtpWriter != nil {
+				fecPackets = f.flexFecEncoder.EncodeFec(f.packetBuffer, 2)
+
+				for _, fecPacket := range fecPackets {
+					fecResult, fecErr := f.fecRtpWriter.Write(&fecPacket.Header, fecPacket.Payload, attributes)
+
+					if fecErr != nil && fecResult == 0 {
+						break
+					}
+				}
+				// Reset the packet buffer now that we've sent the corresponding FEC packets.
+				f.packetBuffer = nil
+			}
+
+			return result, err
+		})
+	} else if strings.HasSuffix(info.MimeType, "/flexfec-03") && info.Attributes.Get("apt_ssrc") != nil {
+		if stream, ok := r.streams[info.Attributes.Get("apt_ssrc").(uint32)]; ok {
+			stream.flexFecEncoder = NewFlexEncoder03(info.PayloadType, info.SSRC)
+			stream.fecRtpWriter = writer
+		}
+	}
+
+	return writer
+}
+
+// UnbindLocalStream is called when the Stream is removed. It can be used to clean up any data related to that track.
+func (r *FecInterceptor) UnbindLocalStream(info *interceptor.StreamInfo) {
+	r.streamsMu.Lock()
+	delete(r.streams, info.SSRC)
+	r.streamsMu.Unlock()
 }
